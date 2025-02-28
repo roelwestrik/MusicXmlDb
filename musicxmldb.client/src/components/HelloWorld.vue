ccb929d07073810e13d04315db99957233d94632
Roel Westrik <roelwestrik@gmail.com>
2025-02-28 19:51:19 +0100

    keycloak

--- musicxmldb.client/src/components/HelloWorld.vue
+++ musicxmldb.client/src/components/HelloWorld.vue
@@ -1,99 +1,49 @@
 <template>
-    <div class="weather-component">
-        <h1>Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
+  <div class="weather-component">
+    <h1>Weather forecast</h1>
+    <p>This component demonstrates fetching data from the server.</p>
 
-        <div v-if="loading" class="loading">
-            Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationvue">https://aka.ms/jspsintegrationvue</a> for more details.
-        </div>
+    <div v-if="loggedIn"><p>Hello, {{ username }}!</p></div>
+    <button v-if="loggedIn" @click="logout">
+      LOGOUT
+    </button>
 
-        <div v-if="post" class="content">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="forecast in post" :key="forecast.date">
-                        <td>{{ forecast.date }}</td>
-                        <td>{{ forecast.temperatureC }}</td>
-                        <td>{{ forecast.temperatureF }}</td>
-                        <td>{{ forecast.summary }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <button v-else @click="login">
+      LOGIN
+    </button>
+
+  </div>
 </template>
 
-<script lang="ts">
-    import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Keycloak from 'keycloak-js';
 
-    type Forecasts = {
-        date: string,
-        temperatureC: string,
-        temperatureF: string,
-        summary: string
-    }[];
+const keycloak = new Keycloak({
+  url: "http://localhost:18080",
+  realm: "musicxmldb-keycloak",
+  clientId: "public-client"
+});
 
-    interface Data {
-        loading: boolean,
-        post: null | Forecasts
-    }
+const loggedIn = ref(false)
+const username = ref("")
 
-    export default defineComponent({
-        data(): Data {
-            return {
-                loading: false,
-                post: null
-            };
-        },
-        created() {
-            // fetch the data when the view is created and the data is
-            // already being observed
-            this.fetchData();
-        },
-        watch: {
-            // call again the method if the route changes
-            '$route': 'fetchData'
-        },
-        methods: {
-            fetchData(): void {
-                this.post = null;
-                this.loading = true;
-
-                fetch('weatherforecast')
-                    .then(r => r.json())
-                    .then(json => {
-                        this.post = json as Forecasts;
-                        this.loading = false;
-                        return;
-                    });
-            }
-        },
+keycloak.init({ onLoad: 'check-sso' }).then(e => {
+  loggedIn.value = e;
+  if (e) {
+    keycloak.loadUserProfile().then(f => {
+      username.value = f.email ?? "";
     });
+  }
+})
+
+const login = async () => {
+  await keycloak.login()
+};
+const logout = async () => {
+  await keycloak.logout();
+};
+
 </script>
 
-<style scoped>
-th {
-    font-weight: bold;
-}
-
-th, td {
-    padding-left: .5rem;
-    padding-right: .5rem;
-}
-
-.weather-component {
-    text-align: center;
-}
-
-table {
-    margin-left: auto;
-    margin-right: auto;
-}
-</style>
+<style scoped></style>
