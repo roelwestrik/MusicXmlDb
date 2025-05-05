7a91c1cd1251eccdaa3b2e961aa0c0a69e59026b
Roel Westrik <roel@Mac.home>
2025-05-05 18:25:08 +0200

    cleanup imslp api

---
+++ MusicXmlDb.IMSLP.API/src/musicxmldb_imslp_api/main.py
@@ -0,0 +1,217 @@
+import os
+import requests
+import logging
+from datetime import datetime
+
+# Set up logging
+logging.basicConfig(
+    filename="logs.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s -  %(message)s"
+)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(console_handler)
+
+class MetaData:
+    start: int
+    limit: int
+    sortby: str
+    sortdirection: str
+    moreresultsavailable: bool
+    timestamp: int
+    apiversion: int
+
+    def __init__(self, dict):
+        self.__dict__.update(dict)
+
+class Composer:
+    id: str
+    type: str
+    parent: str
+    intvals: "IntVals"
+    permlink: str
+
+    def __init__(self, dict):
+        self.__dict__.update(dict)
+        self.intvals = IntVals(dict.get("intvals", {}))
+
+class Work:
+    id: str
+    type: str
+    parent: str
+    intvals: "IntVals"
+    permlink: str
+
+    def __init__(self, dict):
+        self.__dict__.update(dict)
+        self.intvals = IntVals(dict.get("intvals", {}))
+
+class IntVals:
+    composer: str
+    worktitle: str
+    icatno: str
+    pageid: int
+
+    def __init__(self, dict):
+        self.__dict__.update(dict)
+
+def construct_url(base_url, account, disclaimer, sort, type, start, retformat):
+    url = f"{base_url}?account={account}/disclaimer={disclaimer}/sort={sort}/type={type}/start={start}/retformat={retformat}"
+    return url
+
+def parse_metadata(json): 
+    metadata = MetaData(json)
+    return metadata
+
+def parse_composer(json):
+    composer = Composer(json)
+    return composer
+
+def parse_work(json):
+    work = Work(json)
+    return work
+
+def parse_composers_json(json):
+    composers = []
+    metadata = None
+
+    for k, v in json.items():
+        if k == "metadata":
+            metadata = parse_metadata(v)
+        if k.isnumeric():
+            composers.append(parse_composer(v))
+
+    return composers, metadata
+
+def parse_works_json(json):
+    works = []
+    metadata = None
+
+    for k, v in json.items():
+        if k == "metadata":
+            metadata = parse_metadata(v)
+        if k.isnumeric():
+            works.append(parse_work(v))
+
+    return works, metadata
+
+def to_composer_bulk_insert(composers):
+    values = ", ".join(f"('{composer.id}', FALSE)" for composer in composers)
+    bulk_upsert_sql = f"""
+        INSERT INTO composer (name, pending_deletion)
+        VALUES {values}
+        ON CONFLICT (name) DO UPDATE SET pending_deletion = FALSE;
+    """
+    return bulk_upsert_sql
+
+def to_work_bulk_insert(works):
+    values = ", ".join(f"('{work.id}', FALSE)" for work in works)
+    bulk_upsert_sql = f"""
+        INSERT INTO composer (name, pending_deletion)
+        VALUES {values}
+        ON CONFLICT (name) DO UPDATE SET pending_deletion = FALSE;
+    """
+    return bulk_upsert_sql
+
+def get_composers():
+    start = 0
+    has_more_data = True
+    composers = []
+
+    while has_more_data:
+        url = construct_url(
+            base_url="http://imslp.org/imslpscripts/API.ISCR.php",
+            account="worklist",
+            disclaimer="accepted",
+            sort="id",
+            type="1",
+            start=start,
+            retformat="json"
+        )
+        logger.info(f"Getting data from {url}")
+        response = requests.get(url)
+        json = response.json()
+
+        _composers, metadata = parse_composers_json(json)
+        composers += _composers
+
+        has_more_data = metadata.moreresultsavailable
+        start += metadata.limit
+
+        if start > 3000:
+            break
+
+    return composers
+
+def get_works():
+    start = 0
+    has_more_data = True
+    works = []
+
+    while has_more_data:
+        url = construct_url(
+            base_url="http://imslp.org/imslpscripts/API.ISCR.php",
+            account="worklist",
+            disclaimer="accepted",
+            sort="id",
+            type="2",
+            start=start,
+            retformat="json"
+        )
+        logger.info(f"Getting data from {url}")
+        response = requests.get(url)
+        json = response.json()
+
+        _works, metadata = parse_works_json(json)
+        works += _works
+
+        has_more_data = metadata.moreresultsavailable
+        start += metadata.limit
+
+        if start > 3000:
+            break
+    
+    return works
+
+def main():
+    """Fetch paginated API data for composers or works and store each page separately."""
+    try:
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+        filename = f"bulk_update_{timestamp}.sql"
+        logger.info(f"Creating file {filename}.")
+        with open(filename, "w", encoding="utf-8") as sql_file:
+
+            sql_file.write("BEGIN;")
+            sql_file.write(os.linesep)
+            sql_file.write(os.linesep)
+
+            logger.info("Getting composers.")
+            composers = get_composers()
+            bulk_upsert_sql = to_composer_bulk_insert(composers) 
+            sql_file.write(bulk_upsert_sql)
+            sql_file.write(os.linesep)
+
+            logger.info("Getting works.")
+            works = get_works()
+            bulk_upsert_sql = to_work_bulk_insert(works) 
+            sql_file.write(bulk_upsert_sql)
+            sql_file.write(os.linesep)
+
+            sql_file.write("COMMIT;")
+            sql_file.write(os.linesep)
+
+        logger.info("File closed succesfully.")
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching data: {e}")
+        raise
+    finally:
+        logger.info("Exiting application now.")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
